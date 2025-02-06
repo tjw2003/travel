d70ff1f593cd62d342082c0abbfd29121556ed70
tjw <114938661+tjw2003@users.noreply.github.com>
2025-02-06 15:45:12 +0800

    引入tailwind

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'; // 导入路由配置
-
+import './assets/tailwind.css'  // 引入 Tailwind CSS 文件
 
 createApp(App)
   .use(router)  
